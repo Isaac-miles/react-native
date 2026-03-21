a2a2a4dc6dd9b4d473c1f95519bd7213c23f4b7a
isaac-miles <isaacnice247@gmail.com>
2026-03-21 22:44:15 +0100

    update

--- zipcode/app/index.tsx
+++ zipcode/app/index.tsx
@@ -1,5 +1,6 @@
-import { View, Text, StyleSheet,ImageBackground } from 'react-native'
+import { View, Text, StyleSheet,ImageBackground, Pressable } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router';
 
 import icedCoffee from "../assets/images/iced-coffee.png";
 
@@ -10,7 +11,12 @@ const app = () => {
          source={icedCoffee}
          resizeMode='cover'
           style={styles.image}>
-        <Text style={styles.text}>coffee shop</Text>
+        <Text style={styles.title}>coffee shop</Text>
+        <Link href="/contact" style={{marginHorizontal:"auto"}} asChild>
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>Contact Us</Text>
+          </Pressable>
+        </Link>
       </ImageBackground>
     </View>
   )
@@ -24,12 +30,36 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
   },
-  text:{
+  title:{
     color: 'white',
     fontSize: 42,
     fontWeight: 'bold',
     textAlign: 'center',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    marginBottom: 20,
+  },
+  link:{
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+    padding: 4,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  button:{
+    height: 50,
+    borderRadius:20,
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    padding: 6,
+    justifyContent: 'center',      
+  },
+  buttonText:{
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 4,
   },
   image: {
     width: '100%',
